7b38fcf2140c3c03a80fee2a86d1f201b4d08cb8
Antonio Ceballos <aceballos@gmail.com>
2020-03-15 13:17:42 +0000

    Pre-release 6.2.6-pre1

--- src/main.cc
+++ src/main.cc
@@ -391,7 +391,7 @@ Copyright (C) %s Free Software Foundation, Inc.\n\
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n"),
-             "2017" );
+             "2020" );
   }
 
   /* If a usage statement is required output it here */
